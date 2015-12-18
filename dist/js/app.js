962d8a35a33a44cb5c7e8438bdb9d04b2003528d
jming <jming@college.harvard.edu>
2015-12-18 11:19:42 +0530

    fix graphing error

--- dist/js/app.js
+++ dist/js/app.js
@@ -41,7 +41,7 @@ $('#drip-update').click(function() {
 
         // calculate the new values
 
-        inputs.push([year, drip]);
+        inputs.push([Number(year), Number(drip)]);
 
     }
 
@@ -90,9 +90,11 @@ function calc_drip_inputs(inputs) {
     // create series of arrays
     for (var i = 0; i < inputs.length; i++) {
 
+        console.log(inputs[i], inputs[i+1]);
+
         var year_diff = (i == inputs.length - 1) ? (15-inputs[i][0]) : (inputs[i+1][0] - inputs[i][0]);
         var drip_val = calc_drip(inputs[i][1]);
-        // console.log(year_diff, drip_val);
+        console.log(year_diff, drip_val);
         // console.log(repeated_array( year_diff, drip_val ));
         ret = ret.concat(repeated_array( year_diff, drip_val ));
         // console.log(ret);
